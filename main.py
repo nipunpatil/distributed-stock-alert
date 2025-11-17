199bd9448c5eef118bcf05c30b164c168d955511
Siddesh Lohkare <118656715+Siddesh3108@users.noreply.github.com>
2025-11-17 15:37:21 +0530

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-import yfinance as yf
-stock = yf.Ticker("AAPL")
-price = stock.info.get('currentPrice')
-print(price)
-stock1 = yf.Ticker("GOOG")
-price = stock1.info.get('currentPrice')
-print(price)
